7014ca6b519bf94bfab832354c46c2e24cf611f2
Nishant8826 <rnishant609@gmail.com>
2022-07-18 19:30:54 +0530

    modify

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -9,19 +9,19 @@ function isValid(value) {
     return true;
   }
 
-
 const createURL=async(req,res)=>{
     try{
         let {longUrl}=req.body;
 //----------------------------VALIDATION STARTS------------------------//
-    if(!isValid(longUrl))return res.status(400).send({ status: false,message:"Please provide URL"})   
-    const checkURL=await urlModel.findOne({longUrl})
-    if(checkURL)return res.status(400).send({ status: false,message:`${longUrl} is already present`}) 
+        if(!isValid(longUrl))return res.status(400).send({ status: false,message:"Please provide URL"})  
+        if(!validURL.isUri(longUrl))return res.status(400).send({ status: false,message:`URL is not a Valid URL`}) 
+        const checkURL=await urlModel.findOne({longUrl}).select({__v:0,_id:0})
+        if(checkURL)return res.status(200).send({ status: true, data :checkURL}) 
 //---------------------------------------------------------------------//
-        const urlCode=shortId.generate()
-        const baseurl="http://localhost:3000";
-        const shortUrl=`${baseurl}/${urlCode}`
-        let result=new urlModel({longUrl,shortUrl,urlCode})
+        const urlCode=shortId.generate().toLowerCase()
+        const baseURL="http://localhost:3000/"
+        const shortUrl=`${baseURL}${urlCode}`
+        let result={longUrl,shortUrl,urlCode} 
         const newResult=await urlModel.create(result)
         return res.status(201).send({ status: true,data: newResult})
     }catch(error){
@@ -33,9 +33,8 @@ const getByID=async(req,res)=>{
     try{
         let urlCode=req.params.urlCode;
         const result=await urlModel.findOne({urlCode}).select({longUrl:1,_id:0})
-        if(!result)return res.status(404).send({status:false,message:"Not Found"})
-        
-        return res.status(200).send({status:true,data:result})
+        if(!result)return res.status(404).send({status:false,message:"Not Found"})  
+        return res.status(302).redirect(result.longUrl)
     }catch(error){
         return res.status(500).send({ status: false,message: error.message})
     }
--- src/models/urlModel.js
+++ src/models/urlModel.js
@@ -3,7 +3,8 @@ const mongoose=require("mongoose")
 const urlSchema=new mongoose.Schema({
      "urlCode": {type:String, required:true, unique:true, lowercase:true, trim:true }, 
      "longUrl": {type:String,required:true},
-     "shortUrl": {type:String,required:true, unique:true} 
-})
+     "shortUrl": {type:String,required:true, unique:true}
+},{versionKey:false})
 
 module.exports=mongoose.model("url",urlSchema)
+
