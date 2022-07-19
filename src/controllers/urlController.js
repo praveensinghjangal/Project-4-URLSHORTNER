ce86d9113eebbde9564f7c854407be2da06a4ed0
Nishant8826 <rnishant609@gmail.com>
2022-07-19 12:47:03 +0530

    aaaa

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -1,7 +1,10 @@
 const urlModel=require("../models/urlModel")
 const shortId=require("shortid")
 
+
 const rexURL = /(http|https):\/\/(www\.)?[-a-zA-Z0-9@:%.\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%\+.~#?&//=]*)/
+const coderegex =/^[A-Za-z0-9]{7,14}$/
+
 function isValid(value) {
     if (typeof value === "undefined" || typeof value === "null") return false;
     if (typeof value === "string" && value.trim().length === 0) return false;
@@ -10,15 +13,18 @@ function isValid(value) {
 
 const createURL=async(req,res)=>{
     try{
-        let {longUrl}=req.body;
-//----------------------------VALIDATION STARTS------------------------//
-        if(!isValid(longUrl))return res.status(400).send({ status: false,message:"Please provide URL"})  
+
+        let {longUrl,...rest}=req.body;
+//--------------------------------VALIDATION STARTS-----------------------------//
+        if(Object.keys(rest).length>0)return res.status(400).send({ status: false,message:"Invalid attributes"})        
+        if(!isValid(longUrl))return res.status(400).send({ status: false,message:"long url should be present"})  
         if(!longUrl.match(rexURL))return res.status(400).send({ status: false,message:"URL not a Valid "})  
-         
         const checkURL=await urlModel.findOne({longUrl}).select({__v:0,_id:0})
         if(checkURL)return res.status(200).send({ status: true, data :checkURL}) 
-//---------------------------------------------------------------------//
+//------------------------------------------------------------------------------//
         const urlCode=shortId.generate().toLowerCase()
+        const checkShortURl=await urlModel.findOne({urlCode})
+        if(checkShortURl)return res.status(400).send({ status: false,message:"short URl already taken"})
         const baseURL="http://localhost:3000/"
         const shortUrl=`${baseURL}${urlCode}`
         let result={longUrl,shortUrl,urlCode} 
@@ -32,7 +38,6 @@ const createURL=async(req,res)=>{
 const getByID=async(req,res)=>{
     try{
         let urlCode=req.params.urlCode;
-        let coderegex =/^[A-Za-z0-9]{7,14}$/
         if(!urlCode.match(coderegex)) return res.status(400).send({status :false , message : " Invalid urlcode "})
         const result=await urlModel.findOne({urlCode}).select({longUrl:1,_id:0})
         if(!result)return res.status(404).send({status:false,message:" urlcode Not Found"})  
